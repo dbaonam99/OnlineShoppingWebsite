1ab2f786b664f308ad8de805739a489d04425feb
BaoNam99 <obamavn99@gmail.com>
2020-11-23 15:16:07 +0700

    Add responsive

--- frontend/src/components/LoadingPage.js
+++ frontend/src/components/LoadingPage.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../Styles/LoadingPage.css'
 export default function LoadingPage(props) {
 
+    const rootElement = document.querySelector(".root-element")
+    const viewPortH = loading.getBoundingClientRect().height;
+    const windowH = window.innerHeight;
+    const browserUiBarsH = viewPortH - windowH; 
+
     return(
-        <div className={props.loading ? "loading-page" : "loading-page disable-loading"}>
+        <div 
+            className={props.loading ? "loading-page" : "loading-page disable-loading"} 
+            ref={loading} 
+            style={{height: `calc(100vh - ${browserUiBarsH}px)`}}
+        >
             <div className="loading-page-container flex-center">
                 <p className="loading-text hello">Hello!</p>
             </div>
