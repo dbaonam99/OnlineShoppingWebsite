646895346e9f942b15b8839b21cb9908382cd500
BaoNam99 <obamavn99@gmail.com>
2020-11-23 14:56:30 +0700

    Add responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ function App(props) {
     if (window.location.href.split('/')[3] === "") {
       setTimeout(()=>{
         setLoading(false)
-      }, 5500000)
+      }, 5500)
     } else {
       setLoading(false)
     }
