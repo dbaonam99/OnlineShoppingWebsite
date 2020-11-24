468bfb897081ad47ac29dc534926bca975849561
BaoNam99 <obamavn99@gmail.com>
2020-11-24 17:40:03 +0700

    Update responsive for news

--- frontend/src/components/News/News.js
+++ frontend/src/components/News/News.js
@@ -12,6 +12,10 @@ function News(props) {
     const news = props.news;
     const cateLink = (`/news/category/${news.newCate}`).toLowerCase()
 
+    const date = new Date(news.newTime)
+    const day = date.getDate(date)
+    const month = date.getMonth(date) + 1
+    const year = date.getFullYear(date)
     return(
         <div 
             className="News">
@@ -19,22 +23,24 @@ function News(props) {
                 onClick={redirect}>
                 <img src={news.newImg} alt=""></img>
             </div>
-            <div className="news-info flex-center">
-                <div className="news-time">{news.newTime}</div>
-                <Link to={cateLink} className="news-cate" onClick={()=>{window.scrollTo(0,0);}}>{news.newCate}</Link>
-            </div>
-            <div 
-                className="news-title-box"
-                onClick={redirect}>
-                {news.newTitle}
-            </div>
-            <p className="news-content tripledot">
-                {news.newContent}
-            </p>
-            <div 
-                className="news-link flex-center"
-                onClick={redirect}>
-                <p>Read More</p>
+            <div className="news-right">
+                <div className="news-info flex-center">
+                    <div className="news-time">{day}.{month}.{year}</div>
+                    <Link to={cateLink} className="news-cate" onClick={()=>{window.scrollTo(0,0);}}>{news.newCate}</Link>
+                </div>
+                <div 
+                    className="news-title-box"
+                    onClick={redirect}>
+                    {news.newTitle}
+                </div>
+                <div className="news-content"
+                    dangerouslySetInnerHTML={{__html: news.newContent}}>
+                </div>
+                <div 
+                    className="news-link flex-center"
+                    onClick={redirect}>
+                    <p>Read More</p>
+                </div>
             </div>
         </div>
     )
